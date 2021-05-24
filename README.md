9fe10294380e426586854b827df60f47bde5cc0a
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-05-24 14:21:08 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# Winc Student Dashboard
-
------------
-> See live at: <http://winc-student-dashboard.bymelvin.com/>
------------
-
-
-*made during the winc academy front end development course*
-
-
-
 
--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,18 +1,49 @@
+// import React from "react";
+
+// function AllData(props) {
+//   return (
+//     <div>
+//       <table>
+//         <tr>
+//           <th>first name</th>
+//           <th>task</th>
+//           <th>enjoyment</th>
+//           <th>difficulty</th>
+//         </tr>
+//       </table>
+//     </div>
+//   );
+// }
+
+// export default AllData;
+
 import React from "react";
 
-function AllData(props) {
-  return (
-    <div>
-      <table>
-        <tr>
-          <th>first name</th>
-          <th>task</th>
-          <th>enjoyment</th>
-          <th>difficulty</th>
-        </tr>
-      </table>
-    </div>
-  );
-}
+class AllData extends React.Component {
+  render() {
+    const reviews = this.props.reviews;
 
+    return (
+      <div>
+        <table>
+          <tr>
+            <th>first name</th>
+            <th>task</th>
+            <th>enjoyment</th>
+            <th>difficulty</th>
+          </tr>
+          {/* {reviews.map((review) => { */}
+          {/* return ( */}
+          <tr>
+            {/* <td>{this.assignment.name}</td> */}
+            {/* <td>{review.task}</td>
+                <td>{review.enjoymentRating}</td>
+                <td>{review.difficultyRating}</td> */}
+          </tr>
+          {/* ); })} */}
+        </table>
+      </div>
+    );
+  }
+}
 export default AllData;
